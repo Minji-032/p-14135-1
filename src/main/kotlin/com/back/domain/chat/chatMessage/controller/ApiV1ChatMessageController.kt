73ab94defe794fb977c116a1580675aff2b8ec01
Minji-032 <5h6vmm@gmail.com>
2026-02-11 10:36:43 +0900

    SSE+세팅

---
+++ src/main/kotlin/com/back/domain/chat/chatMessage/controller/ApiV1ChatMessageController.kt
@@ -0,0 +1,113 @@
+package com.back.domain.chat.chatMessage.controller
+
+import com.back.domain.chat.chatMessage.entity.ChatMessage
+import com.back.global.sse.SseEmitters
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+
+@RestController
+@RequestMapping("/api/v1/chat/rooms/{chatRoomId}/messages")
+@CrossOrigin(origins = ["https://cdpn.io"])
+class ApiV1ChatMessageController(
+    private val sseEmitters: SseEmitters
+) {
+    private var lastChatMessageId = 0
+
+    private val chatMessagesByRoomId: MutableMap<Int, MutableList<ChatMessage>> = mutableMapOf(
+        1 to mutableListOf(
+            ChatMessage(
+                ++lastChatMessageId,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                1,
+                "김철수",
+                "풋살하실 분 계신가요?"
+            ),
+            ChatMessage(
+                ++lastChatMessageId,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                1,
+                "이영희",
+                "네, 저요!"
+            )
+        ),
+        2 to mutableListOf(
+            ChatMessage(
+                ++lastChatMessageId,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                2,
+                "박철수",
+                "농구하실 분 계신가요?"
+            ),
+            ChatMessage(
+                ++lastChatMessageId,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                2,
+                "김영희",
+                "네, 저요!"
+            )
+        ),
+        3 to mutableListOf(
+            ChatMessage(
+                ++lastChatMessageId,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                3,
+                "이철수",
+                "야구하실 분 계신가요?"
+            ),
+            ChatMessage(
+                ++lastChatMessageId,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                3,
+                "박영희",
+                "네, 저요!"
+            )
+        )
+    )
+
+
+    @GetMapping
+    fun getItems(
+        @PathVariable chatRoomId: Int,
+        @RequestParam(defaultValue = "-1") afterChatMessageId: Int,
+    ): List<ChatMessage> =
+        chatMessagesByRoomId[chatRoomId]
+            ?.filter { it.id > afterChatMessageId }
+            ?.toList()
+            ?: emptyList()
+
+
+    data class ChatMessageWriteReqBody(
+        val writerName: String,
+        val content: String
+    )
+
+    @PostMapping
+    fun write(
+        @PathVariable chatRoomId: Int,
+        @RequestBody reqBody: ChatMessageWriteReqBody
+    ): ChatMessage {
+        val chatMessages = chatMessagesByRoomId.getOrPut(chatRoomId) { mutableListOf() }
+
+        val chatMessage = ChatMessage(
+            ++lastChatMessageId,
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            chatRoomId,
+            reqBody.writerName,
+            reqBody.content
+        )
+
+        chatMessages.add(chatMessage)
+
+        //메시지 작성 이벤트마다 SSE 발행
+        sseEmitters.send("chat__room__$chatRoomId", "chat__messageCreated", chatMessage)
+
+        return chatMessage
+    }
+}
---
+++ src/main/kotlin/com/back/domain/sse/controller/SseController.kt
@@ -0,0 +1,19 @@
+package com.back.domain.sse.controller
+
+import com.back.global.sse.SseEmitters
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+import java.io.IOException
+
+@RestController
+@RequestMapping("/sse")
+@CrossOrigin(origins = ["https://cdpn.io"])
+class SseController(
+    private val sseEmitters: SseEmitters
+) {
+    @GetMapping("/connect/{key}", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun connect(@PathVariable key: String): SseEmitter {
+        return sseEmitters.connect(key)
+    }
+}
